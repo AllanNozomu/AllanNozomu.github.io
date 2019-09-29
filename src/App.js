17eff50c26889dceb4dd3f83b0b4cad1d6718714
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2019-09-29 20:51:34 -0300

    Add icons texts and git status

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+
+library.add(fab, faHeart)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <div className="content">
         <h1 className="Header">Allan Nozomu</h1>
-        <h1 className="SubHeader">I am a Fullstack developer</h1>
-      </header>
+        <h3 className="SubHeader">Django Fullstack developer</h3>
+        <div className="App-body">
+          <a href="https://linkedin.com/in/allan-nozomu/" target="#">
+            <FontAwesomeIcon  className="footer-item" icon={['fab', 'linkedin']} />
+          </a>
+          <a href="https://github.com/allannozomu" target="#">
+            <FontAwesomeIcon  className="footer-item" icon={['fab', 'github']} />
+          </a>
+        </div>
+      </div>
+
+      <footer className="App-footer">
+        <p>Created with <FontAwesomeIcon icon="heart" color="red" /> by <a href="https://github.com/allannozomu/my-site" target="#">allannozomu</a></p>
+      </footer>
     </div>
   );
 }

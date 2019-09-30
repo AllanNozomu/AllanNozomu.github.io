ce3fa4402e7bd635905d2e3e4b2fca353c755336
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-30 23:23:55 +0000

    Deploy AllanNozomu/my-site to github.com/AllanNozomu/my-site.git:gh-pages

---
+++ precache-manifest.a9f3b5dd2151e00af913f5044d5e0346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068d8528ae862e2f501fdbdb57634da9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2faab425e4547fb1fa0",
+    "url": "/static/css/main.4ea05db6.chunk.css"
+  },
+  {
+    "revision": "f3f39f87c3ec98ee5efd",
+    "url": "/static/js/2.0e47130c.chunk.js"
+  },
+  {
+    "revision": "f2faab425e4547fb1fa0",
+    "url": "/static/js/main.d3688262.chunk.js"
+  },
+  {
+    "revision": "e4c3999116911165ca6e",
+    "url": "/static/js/runtime-main.b1d4afee.js"
+  },
+  {
+    "revision": "4290ea77f18e51028fc4eb0b77136e7e",
+    "url": "/static/media/BebasNeue-Regular.4290ea77.ttf"
+  },
+  {
+    "revision": "e663748100841006a7ab9da9d3e49a9a",
+    "url": "/static/media/Cardo-Regular.e6637481.ttf"
+  }
+]);
